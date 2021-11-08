8ec8f5fc6b536aa5ecc7bb792ac6f92d475e5c89
Nebularswamp <grosswamp@gmail.com>
2021-11-07 19:02:26 -0500

    Added original project
    I made SMS like a year ago or sth, now thinking of making a open source version of this application using php and ajax so that it's possible for clients to contribute data / save results to the server

---
+++ Surreal Meme Sandbox/code.js
@@ -0,0 +1,153 @@
+
+function generate(){
+
+  checkImageType();
+  checkTopText();
+  checkButtomText();
+
+  document.getElementById("myText1").innerHTML = document.getElementById("myText").innerHTML;
+  document.getElementById("mybText1").innerHTML = document.getElementById("mybText").innerHTML;
+
+  generatePic();
+}
+
+function checkImageType(){
+  var atmo = document.getElementById("atmo");
+  var anime = document.getElementById("anime");
+  var thing = document.getElementById("thing");
+  var stock = document.getElementById("stock");
+  if(atmo.checked ===true){
+    randomAtmoImage();
+  }else if(anime.checked === true){
+    randomAnimeImage();
+  }else if(stock.checked === true){
+    randomStockImage();
+  }else if(thing.checked === true){
+    randomThingImage();
+  }else{
+    randomAllImage();
+  }
+}
+
+function randomAtmoImage(){
+  var randomNumImg = Math.floor(Math.random()* atmoImg.length);
+  document.getElementById("myMeme").src = atmoImg[randomNumImg];
+}
+
+function randomAnimeImage(){
+  var randomNumImg = Math.floor(Math.random()* animeImg.length);
+  document.getElementById("myMeme").src = animeImg[randomNumImg];
+}
+
+function randomStockImage(){
+  var randomNumImg = Math.floor(Math.random()* stockImg.length);
+  document.getElementById("myMeme").src = stockImg[randomNumImg];
+}
+
+function randomThingImage(){
+  var randomNumImg = Math.floor(Math.random()* thingImg.length);
+  document.getElementById("myMeme").src = thingImg[randomNumImg];
+}
+
+function randomAllImage(){
+  var randomNumImg = Math.floor(Math.random()* allImg.length);
+  document.getElementById("myMeme").src = allImg[randomNumImg];
+}
+
+
+function checkTopText(){
+  var question = document.getElementById("question");
+  var exclamation = document.getElementById("exclamation");
+  var greeting = document.getElementById("greeting");
+  var custom = document.getElementById("custom");
+  if(question.checked ===true){
+    randomQuestionTopText();
+  }else if(exclamation.checked === true){
+    randomExclamationTopText();
+  }else if(greeting.checked === true){
+    randomGreetingTopText();
+  }else if(custom.checked === true){
+    customTopText();
+  }else{
+    randomAllTopText();
+  }
+}
+
+function randomQuestionTopText(){
+  randomNumTxt = Math.floor(Math.random()* questionTxt.length);
+  document.getElementById("myText").innerHTML = questionTxt[randomNumTxt];
+}
+
+function randomGreetingTopText(){
+  randomNumTxt = Math.floor(Math.random()* greetingTxt.length);
+  document.getElementById("myText").innerHTML = greetingTxt[randomNumTxt];
+}
+
+function randomExclamationTopText(){
+  randomNumTxt = Math.floor(Math.random()* exclamationTxt.length);
+  document.getElementById("myText").innerHTML = exclamationTxt[randomNumTxt];
+}
+
+function randomAllTopText(){
+  randomNumTxt = Math.floor(Math.random()* allTxt.length);
+  document.getElementById("myText").innerHTML = allTxt[randomNumTxt];
+}
+
+function customTopText(){
+  document.getElementById("myText").innerHTML = document.getElementById("customtoptext").value;
+}
+
+
+function checkButtomText(){
+  var bquestion = document.getElementById("bquestion");
+  var bexclamation = document.getElementById("bexclamation");
+  var bgreeting = document.getElementById("bgreeting");
+  var bcustom = document.getElementById("bcustom");
+  if(bquestion.checked ===true){
+    randomQuestionBText();
+  }else if(bexclamation.checked === true){
+    randomExclamationBText();
+  }else if(bgreeting.checked === true){
+    randomGreetingBText();
+  }else if(bcustom.checked === true){
+    customBText();
+  }else{
+    randomAllBText();
+  }
+}
+
+function randomQuestionBText(){
+  randomNumTxt2 = Math.floor(Math.random()* questionTxt.length);
+  document.getElementById("mybText").innerHTML = questionTxt[randomNumTxt2];
+}
+
+function randomGreetingBText(){
+  randomNumTxt2 = Math.floor(Math.random()* greetingTxt.length);
+  document.getElementById("mybText").innerHTML = greetingTxt[randomNumTxt2];
+}
+
+function randomExclamationBText(){
+  randomNumTxt2 = Math.floor(Math.random()* exclamationTxt.length);
+  document.getElementById("mybText").innerHTML = exclamationTxt[randomNumTxt2];
+}
+
+function randomAllBText(){
+  randomNumTxt2 = Math.floor(Math.random()* allTxt.length);
+  document.getElementById("mybText").innerHTML = allTxt[randomNumTxt2];
+}
+
+function customBText(){
+  document.getElementById("mybText").innerHTML = document.getElementById("custombuttomtext").value;
+}
+
+
+function generatePic() {
+  html2canvas(document.getElementById("imagewrap"), {
+    onrendered: function(canvas) {
+      canvas.className = "html2canvas";
+      var image = canvas.toDataURL("image/png");
+      document.getElementById("downloadLink").href = image;
+    },
+    useCORS: true
+  });
+}
---
+++ Surreal Meme Sandbox/resources.js
@@ -0,0 +1,82 @@
+//holds data
+
+var animeImg = new Array(
+  "images/anime1.png",
+  "images/anime2.png",
+  "images/anime3.png",
+  "images/anime4.png",
+  "images/anime5.png",
+  "images/anime6.png",
+  "images/anime7.png",
+  "images/anime8.png",
+  "images/anime9.png",
+  "images/anime10.png",);
+
+var atmoImg = new Array(
+  "images/atmo1.png",
+  "images/atmo2.png",
+  "images/atmo3.png",
+  "images/atmo4.png",
+  "images/atmo5.png",
+  "images/atmo6.png",
+  "images/atmo7.png",
+  "images/atmo8.png",
+  "images/atmo9.png",
+  "images/atmo10.png",
+)
+
+var stockImg = new Array(
+  "images/stock1.png",
+  "images/stock2.png",
+  "images/stock3.png",
+  "images/stock4.png",
+  "images/stock5.png",
+  "images/stock6.png",
+  "images/stock7.png",
+  "images/stock8.png",
+  "images/stock9.png",
+  "images/stock10.png",
+)
+
+var thingImg = new Array(
+  "images/thing1.png",
+  "images/thing2.png",
+  "images/thing3.png",
+  "images/thing4.png",
+  "images/thing5.png",
+  "images/thing6.png",
+  "images/thing7.png",
+  "images/thing8.png",
+  "images/thing9.png",
+  "images/thing10.png",
+)
+
+var allImg = [].concat(thingImg,stockImg,animeImg,atmoImg);
+
+var greetingTxt = new Array(
+  "HELLO",
+  "GOOBYE",
+  "GREETINGS!",
+  "HELLO ITS ME",
+  "H I ",
+  "MAY I HAVE A MINUTE WITH YOU");
+
+var questionTxt = new Array(
+  "WHAT IS MY PURPOSE",
+  "WHY ARE WE HERE",
+  "WHAT IS LIFE",
+  "WHY ARE WE HERE",
+  "WHYYYYY",
+  "SHOULD I BUY STOCKS",
+  "WHY ARE YOU LIKE THIS");
+
+var exclamationTxt = new Array(
+  "OHHHH WEEEEEEEEEE",
+  "AAAAAAAAAAA",
+  "YEEHAAAWWW",
+  "LMAO",
+  "FUCK YEEEEEEAAAAA",
+  "OH NO",
+  "*SCREACHING IN PAIN*");
+
+var allTxt = [].concat(greetingTxt,questionTxt,exclamationTxt);
